7dbbff15f9c0e4be952de5bdfa2d94a5210cbf64
Ryan Merritt <merrittaudio@gmail.com>
2023-04-27 08:07:07 -0500

    add instruction kind enum and memory instruction kinds

--- src/sim.h
+++ src/sim.h
@@ -7,3 +7,14 @@ typedef int32_t s32;
 
 typedef size_t size;
 
+typedef enum
+{
+   instruction_kind_RegisterMemoryToFromRegister,
+   instruction_kind_ImmediateToRegisterMemory,
+   instruction_kind_ImmediateToRegister,
+   instruction_kind_MemoryToAccumulator,
+   instruction_kind_AccumulatorToMemory,
+   instruction_kind_RegisterMemoryToSegmentRegister,
+   instruction_kind_RegisterToRegisterMemory,
+   /* TODO: fill out the rest of the instructions */
+} instruction_kind;
