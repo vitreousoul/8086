3eab5d14b06c862ae3cdca7cc3fc8201eee5e922
Ryan Merritt <merrittaudio@gmail.com>
2023-05-22 07:54:04 -0500

    begin opcode table for memory instructions

--- src/sim.c
+++ src/sim.c
@@ -1,6 +1,41 @@
 #include "sim.h"
 #include "platform.c"
 
+#define OPCODE_MASK 0b111111
+#define GET_OPCODE(b) (((b) >> 2) & OPCODE_MASK)
+
+#define OPCODE_TAIL_MASK 0b11
+/* TODO: should GET_OPCODE_TAIL shift down? isn't the tail at the end, and so we just mask without any shifts? */
+#define GET_OPCODE_TAIL(b) ((b) & OPCODE_MASK)
+
+#define OPCODE_BIT_WIDTH 6
+#define OPCODE_TABLE_COUNT (1 << OPCODE_BIT_WIDTH)
+s32 OpcodeTable[OPCODE_TABLE_COUNT] = {
+    [0b100010] = instruction_kind_RegisterMemoryToFromRegister,
+    [0b110001] = instruction_kind_ImmediateToRegisterMemory,
+    [0b101100] = instruction_kind_ImmediateToRegister,
+    [0b101101] = instruction_kind_ImmediateToRegister,
+    [0b101110] = instruction_kind_ImmediateToRegister,
+    [0b101111] = instruction_kind_ImmediateToRegister,
+    [0b101000] = instruction_kind_MemoryAccumulator,
+    [0b100011] = instruction_kind_SegmentRegister,
+};
+
+static s32 InstructionSize(u8 Byte)
+{
+    s32 Opcode = GET_OPCODE(Byte);
+    s32 OpcodeTail = GET_OPCODE_TAIL(Byte);
+
+}
+
+static void DecodeInstructionBuffer(buffer *InstructionBuffer)
+{
+    s32 I = 0;
+    while(I < InstructionBuffer->Size)
+    {
+    }
+}
+
 /*
   NOTE: for now we will emit assembly instructions from the instruction buffer, to test if we
   properly interpreted the byte-stream
--- src/sim.h
+++ src/sim.h
@@ -12,9 +12,8 @@ typedef enum
    instruction_kind_RegisterMemoryToFromRegister,
    instruction_kind_ImmediateToRegisterMemory,
    instruction_kind_ImmediateToRegister,
-   instruction_kind_MemoryToAccumulator,
-   instruction_kind_AccumulatorToMemory,
-   instruction_kind_RegisterMemoryToSegmentRegister,
+   instruction_kind_MemoryAccumulator,
+   instruction_kind_SegmentRegister,
    instruction_kind_RegisterToRegisterMemory,
    /* TODO: fill out the rest of the instructions */
 } instruction_kind;
